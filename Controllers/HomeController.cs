22e41c95f6caf82785a829bbf52078b7d3dc2bed
codesaitama <achanamosey@gmail.com>
2019-12-28 04:03:50 +0000

    Created a GET request that returnss with Json data.

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -1,21 +1,25 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.Diagnostics;
-using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 using AkwaabaCrafts.Models;
+using System.IO;
+using System.Text.Json;
+using Microsoft.AspNetCore.Hosting;
+using AkwaabaCrafts.Services;
 
 namespace AkwaabaCrafts.Controllers
 {
     public class HomeController : Controller
     {
+
+        public IWebHostEnvironment WebHostEnvironment { get; }
         private readonly ILogger<HomeController> _logger;
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(ILogger<HomeController> logger, IWebHostEnvironment webHostEnvironment)
         {
             _logger = logger;
+            WebHostEnvironment = webHostEnvironment;
         }
 
         public IActionResult Index()
@@ -26,6 +30,32 @@ namespace AkwaabaCrafts.Controllers
         public IActionResult Privacy()
         {
             return View();
+        }
+
+        private string JsonFileName
+        {
+            get { return Path.Combine(WebHostEnvironment.WebRootPath, "data", "products.json"); }
+        }
+
+        [Route("api/products")]
+        [HttpGet]
+        public IEnumerable<Products> GetProducts()
+        {
+            using(var jsonFileReader = System.IO.File.OpenText(JsonFileName))
+            {
+                return JsonSerializer.Deserialize<Products[]>(jsonFileReader.ReadToEnd(),
+                    new JsonSerializerOptions
+                    {
+                        PropertyNameCaseInsensitive = true
+                    });
+            }
+        }
+
+        [Route("api/getproducts")]
+        [HttpGet]
+        public IEnumerable<Products> GetProd(){
+            ProductJsonService productJson = new ProductJsonService();
+            return productJson.GetProducts();
         }
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
