b75bf740688db93b24f084403ddeb8c43315d9bb
codesaitama <achanamosey@gmail.com>
2019-12-28 04:05:50 +0000

    Created a master function for making API request in the javascript.

--- wwwroot/js/site.js
+++ wwwroot/js/site.js
@@ -2,3 +2,19 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+
+const apiRequest = function(url, method, data = ''){
+    try{
+      return fetch(url, {
+          method: method,
+          headers: {
+            Accept: 'application/json',
+            'Content-Type': 'application/json',
+          },
+          body: JSON.stringify(data),
+        });
+    }catch (error) {
+      console.error(error);
+    }
+  
+  }
