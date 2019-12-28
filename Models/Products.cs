dedfd3fa1c3f7b5dad19b1083eb5ae13fe344dd2
codesaitama <achanamosey@gmail.com>
2019-12-28 03:24:17 +0000

    Added the class for the data Model for the Products data

---
+++ Models/Products.cs
@@ -0,0 +1,17 @@
+using System;
+using System.Text.Json.Serialization;
+
+namespace Akwaaba.Models
+{
+    public class Products{
+        public string Id { get; set; }
+        public string Maker { get; set; }
+
+        [JsonPropertyName("img")]
+        public string Image { get; set; }
+        public string Url { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public int[] Ratings { get; set; }
+    }
+}
